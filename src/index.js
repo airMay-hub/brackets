3ce9929a0570b7797b37fe61268fc6c08e9fcaa4
airMay-hub <kiiikaaa.93@mail.ru>
2021-08-25 12:57:01 +0300

    feat: solve part of task 'Brackets'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const BRACKETS_PAIR = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    BRACKETS_PAIR[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  };
+
+  const OPEN_BRACKETS = [];
+  for (const value of Object.values(BRACKETS_PAIR)) {
+    OPEN_BRACKETS.push(value);
+  }
+  
+  let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let current = str[i];
+      
+      if (OPEN_BRACKETS.includes(current)) {
+        stack.push(current);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+
+        let top = stack[stack.length - 1];
+
+        if (BRACKETS_PAIR[current] === top) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+
+    return stack.length === 0;
 }
